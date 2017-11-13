4be6b6b2d9e06c8a0d0568ed0ef663dbb9bccbf4
computermouth <computermouth@crunchbangplusplus.org>
2017-11-13 12:43:24 -0800

    Built, flashed, stuck at SPL

--- board/nextthingco/chippro/boot.cmd
+++ board/nextthingco/chippro/boot.cmd
@@ -1,5 +1,6 @@
 nand erase.chip
 mw ${scriptaddr} 0x0
-sleep 4
+nand write.raw.noverify 0x43000000 0 40
+nand write.raw.noverify 0x43000000 40000 40
 fastboot 0
 reset
---
+++ board/nextthingco/chippro/flash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+sunxi-fel spl sunxi-spl.bin
+sleep 2
+
+sunxi-fel write 0x4a000000 u-boot.bin
+sunxi-fel write 0x43000000 sunxi-spl-with-ecc.bin
+sunxi-fel write 0x43100000 boot.scr
+sunxi-fel exe 0x4a000000
+sleep 8
+
+fastboot -i 0x1f3a erase uboot
+fastboot -i 0x1f3a erase uboot-backup
+fastboot -i 0x1f3a erase UBI
+
+fastboot -i 0x1f3a flash uboot u-boot.bin
+fastboot -i 0x1f3a flash uboot-backup u-boot.bin
+fastboot -i 0x1f3a flash UBI rootfs.ubi.sparse
+fastboot -i 0x1f3a continue
